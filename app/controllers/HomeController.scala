7a412219a89d853950adfc6368363bb13cff7016
Alexander <lapseofreason17@gmail.com>
2020-09-30 18:00:03 +0300

    update index method

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,32 +2,21 @@ package controllers
 
 import javax.inject._
 import models.TodolistItem
-import play.api._
-import play.api.mvc._
+import reactivemongo.api.ReadPreference
+import reactivemongo.play.json.collection.JSONCollection
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.Logger
+import play.api.mvc.{AbstractController, ControllerComponents}
 import play.modules.reactivemongo.{
   MongoController,
   ReactiveMongoApi,
   ReactiveMongoComponents
 }
-import play.mvc.Controller
-import reactivemongo.api.commands.WriteResult
-import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.api.Cursor
+import reactivemongo.play.json._, collection._
+import play.api.mvc._
+import play.api.libs.json._
 
-import scala.concurrent.{ExecutionContext, Future}
-//
-import play.api.Logger
-import play.api.mvc.{AbstractController, ControllerComponents}
-
-import play.modules.reactivemongo.{ // ReactiveMongo Play2 plugin
-  MongoController,
-  ReactiveMongoApi,
-  ReactiveMongoComponents
-}
-
-/**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
-  */
 @Singleton
 class HomeController @Inject() (
     override val controllerComponents: ControllerComponents,
@@ -37,21 +26,47 @@ class HomeController @Inject() (
     with ReactiveMongoComponents {
 
   val logger: Logger = Logger(this.getClass)
-
   implicit def ec: ExecutionContext = controllerComponents.executionContext
-
   def collection: Future[JSONCollection] =
-    database.map(_.collection[JSONCollection]("persons"))
+    database.map(_.collection[JSONCollection]("todo-list-items"))
 
+  //GET
   def index(): Action[AnyContent] =
     Action.async { implicit request: Request[AnyContent] =>
       {
-        logger.info("Entered index method")
-        val item = TodolistItem("test2")
-        val futureResult: Future[WriteResult] =
-          collection.flatMap(_.insert.one(item)) //insert.one(item))
-        futureResult.map(_ => Ok(views.html.index(item)))
-        //Ok(views.html.index(item))
+        // get a cursor over not deleted items from MongoDB
+        val cursor: Future[Cursor[TodolistItem]] = collection
+          .map {
+            _.find(
+              Json.obj("isDeleted" -> false),
+              projection = Option.empty[TodolistItem]
+            ).cursor[TodolistItem](ReadPreference.primary)
+          }
+
+        // gather JsObjects from the db in a list
+        val futurePersonsList: Future[List[TodolistItem]] =
+          cursor.flatMap(
+            _.collect[List](-1, Cursor.FailOnError[List[TodolistItem]]())
+          )
+
+        // pass the list of items
+        futurePersonsList.map { items =>
+          Ok(views.html.index(items))
+        }
       }
+    }
+
+  //POST
+  def addItem(): Action[JsValue] =
+    Action.async(parse.json) { request =>
+      request.body
+        .validate[TodolistItem]
+        .map { item =>
+          collection.flatMap(_.insert.one(item)).map { lastError =>
+            logger.debug(s"Successfully inserted with LastError: $lastError")
+            Created
+          }
+        }
+        .getOrElse(Future.successful(BadRequest("invalid json")))
     }
 }
