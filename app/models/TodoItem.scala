4db5fc5ca3215d8c01a2d492b4f22340442bef76
Alexander <lapseofreason17@gmail.com>
2020-10-13 11:19:08 +0300

    update JSON Writes for TodoItem

--- app/models/TodoItem.scala
+++ app/models/TodoItem.scala
@@ -4,7 +4,7 @@ import org.bson.types.ObjectId
 import org.joda.time.DateTime
 import play.api.libs.json.JodaReads._
 import play.api.libs.json._
-import play.api.libs.json.{Reads, JsPath, Json, JsonConfiguration, OWrites}
+import play.api.libs.json.{Reads, Json, JsonConfiguration, OWrites}
 import play.api.libs.json.JsonNaming.SnakeCase
 import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
 import reactivemongo.api.bson.{BSONDocumentHandler, Macros}
@@ -22,22 +22,26 @@ case class TodoItem(
 }
 
 trait TodoItemJson {
-  implicit val todoItemWrites: Writes[TodoItem] = (o: TodoItem) => {
+  val oidReads = Reads[ObjectId](js => js.validate[String].map(new ObjectId(_)))
+  implicit val jodaDateWrites: Writes[DateTime] = new Writes[DateTime] {
+    def writes(d: DateTime): JsValue = JsString(d.toString())
+  }
+
+  implicit val writes: Writes[TodoItem] = (o: TodoItem) =>
     Json.obj(
       "id" -> o.id,
       "name" -> o.name,
-      "is_completed" -> o.isCompleted.toString,
-      "created" -> o.created.toString,
-      "updated" -> o.updated.toString
+      "is_completed" -> o.isCompleted,
+      "created" -> Json.toJson(o.created),
+      "updated" -> Json.toJson(o.updated)
     )
-  }
 
-  implicit val todoItemReads: Reads[TodoItem] = (
-    (JsPath \ "id").read[String].map(new ObjectId(_)) and
-      (JsPath \ "name").read[String] and
-      (JsPath \ "is_completed").read[Boolean] and
-      (JsPath \ "created").read[DateTime] and
-      (JsPath \ "updated").read[DateTime]
+  implicit val reads: Reads[TodoItem] = (
+    (__ \ "id").read[ObjectId](oidReads) and
+      (__ \ "name").read[String] and
+      (__ \ "is_completed").read[Boolean] and
+      (__ \ "created").read[DateTime] and
+      (__ \ "updated").read[DateTime]
   )(TodoItem.apply _)
 }
 
